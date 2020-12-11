96a128b710d71a2869876795abd32c914b273458
Oscar Mollé <scarmoll@gmail.com>
2020-12-11 13:17:29 +0100

    init (level 4 example)

---
+++ app/src/main/java/com/example/madlevel4example/ReminderDao.kt
@@ -0,0 +1,21 @@
+package com.example.madlevel4example
+
+import androidx.room.*
+import com.example.madlevel2example.Reminder
+
+@Dao
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminderTable")
+    suspend fun getAllReminders(): List<Reminder>
+
+    @Insert
+    suspend fun insertReminder(reminder: Reminder)
+
+    @Delete
+    suspend fun deleteReminder(reminder: Reminder)
+
+    @Update
+    suspend fun updateReminder(reminder: Reminder)
+
+}
